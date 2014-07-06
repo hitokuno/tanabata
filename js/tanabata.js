dd6cfa271a22924ec329ced9c4d568e3acee28bb
Hirokazu Tokuno <hitokuno@aol.com>
2014-07-06 16:57:32 +0900

    almost ready for demo
    Replace pictures
    Enhance move
    Refactoring

--- js/tanabata.js
+++ js/tanabata.js
@@ -3,7 +3,7 @@ var range_m=500;
 var range_deg=range_m/one_deg;
 var data_url="./data/tanabata.txt";
 var currentIndex=1;
-var skipMax=4;
+var skipMax=0;
 var lat;
 var lng;
 var GPS_interval_msec=120*1000;
@@ -11,58 +11,61 @@ var fall_interval_msec=100;
 var swing_speed=10;
 var fall_speed=5;
 var width_speed=0.1;
-var x=100,y=0,t=0;
+var x=window.innerWidth/2,y=0,t=0;
 
-$("#container").height(window.innerHeight-$("footer").height());
+$("#container").height(window.innerHeight-$("footer_").height());
 //navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 getFile();
-setInterval("falling()",100);
 
-function falling() {
-  x=x+Math.sin(t) * swing_speed;
-  y=y+fall_speed;
-  var tanzaku=$(".tanzaku");
-  if(y>window.innerHeight-tanzaku.height()) {
-    y=0;
-    getFile();
-  }
-  t=t+fall_speed;
-  tanzaku.css("top",y+'px');
-  tanzaku.css("left",x+'px');
-//  tanzaku.css("width",(1+Math.sin(t*3))*50+'px');
-/*
-*/
+var Tanzaku = function(x,y,t,Img) {
+  this.x=x;
+  this.y=y;
+  this.t=t;
+  this.Img=Img;
+
+  this.falling = function (i){
+    var that = this;
+    var element = $("#t1");
+
+    that.x=that.x+(Math.sin(this.t+ t) ) * swing_speed;
+//    if (that.y>=window.innderHeight-that.y) {
+    if (that.y>=400) {
+      that.y=0;
+      that.t=that.t-Math.random();
+      getFile();
+    } else {
+      that.y=that.y+fall_speed;
+    }
+    console.log(that.y);
+    that.t=that.t+width_speed;
+    element.css("top",that.y-200+'px');
+    element.css("height",'200px');
+    element.css("left",that.x+'px');
+    element.css("width",(1+Math.sin(that.t*1.03))*50+'px');
+
+    setTimeout(function(){
+      that.falling(i);
+    }, 100);
+  };
+};
+
+
+var a = new Tanzaku(x,0,Math.random()+1,'aka_01.jpg');
+a.falling(0);
+
+function connect_device() {
+  document.location="app://recog";
 }
 
-function successCallback(position) {
-  lat = position.coords.latitude;
-  lng = position.coords.longitude;
-  $(".lat").html(lat);
-  $(".lng").html(lng);
+function change_text(message) {
+  document.getElementById('result').innerHTML = message;
+  document.location="app://default";
 }
 
-/***** 位置情報が取得できない場合 *****/
-function errorCallback(error) {
-  var err_msg = "";
-  switch(error.code)
-  {
-    case 1:
-    err_msg = "位置情報の利用が許可されていません";
-    break;
-    case 2:
-    err_msg = "デバイスの位置が判定できません";
-    break;
-    case 3:
-    err_msg = "タイムアウトしました";
-    break;
-  }
-
-  setTimeout(navigator.geolocation.getCurrentPosition(successCallback, errorCallback), GPS_interval_msec);
-}
 
 function getFile() {
   jQuery.ajax({
-    async: false,
+    async: true,
     url: data_url,
     cache: false,
     dataType: 'text'
